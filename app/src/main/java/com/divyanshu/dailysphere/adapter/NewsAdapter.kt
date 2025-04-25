32c3d4eca8c0b97dc59162515187b713228cbfa6
div-tom1506 <divyanshuk1506@gmail.com>
2025-04-25 21:40:43 +0530

    created NewsAdapter file for displaying news

--- app/src/main/java/com/divyanshu/dailysphere/adapter/NewsAdapter.kt
+++ app/src/main/java/com/divyanshu/dailysphere/adapter/NewsAdapter.kt
@@ -1,7 +1,48 @@
 package com.divyanshu.dailysphere.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.divyanshu.dailysphere.R
 import com.divyanshu.dailysphere.model.Article
 
-class NewsAdapter (private var articles: List<Article>){
+class NewsAdapter(private var articles: List<Article>) :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
+    class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.newsTitle)
+        val desc: TextView = view.findViewById(R.id.newsDescription)
+        val image: ImageView = view.findViewById(R.id.newsImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_news, parent, false)
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = articles[position]
+        holder.title.text = article.title
+        holder.desc.text = article.description ?: "No description available"
+        Glide.with(holder.itemView.context)
+            .load(article.imageUrl)
+            .into(holder.image)
+    }
+
+    override fun getItemCount(): Int = articles.size
+
+    fun updateNews(newArticles: List<Article>) {
+        articles = newArticles
+        notifyDataSetChanged()
+    }
+
+    fun appendNews(newArticles: List<Article>) {
+        articles = articles + newArticles
+        notifyDataSetChanged()
+    }
 }
