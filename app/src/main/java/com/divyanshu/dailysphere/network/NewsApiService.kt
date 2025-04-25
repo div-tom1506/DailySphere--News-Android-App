306fdf58800c3107e3a8eb1d77d1e73c09826a60
div-tom1506 <divyanshuk1506@gmail.com>
2025-04-25 20:14:20 +0530

    developed NewsApiService file

--- app/src/main/java/com/divyanshu/dailysphere/network/NewsApiService.kt
+++ app/src/main/java/com/divyanshu/dailysphere/network/NewsApiService.kt
@@ -1,4 +1,17 @@
 package com.divyanshu.dailysphere.network
 
-class NewsApiService {
+import com.divyanshu.dailysphere.model.NewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApiService {
+    @GET("api/1/news")
+    fun getNews(
+        @Query("apiKey") apiKey: String,
+        @Query("q") query: String,
+        @Query("language") language: String = "en",
+        @Query("country") country: String = "us",
+        @Query("page") page: Int = 1
+    ): Call<NewsResponse>
 }
